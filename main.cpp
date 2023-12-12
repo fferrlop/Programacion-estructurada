642fde9c695c8f38cbbd1def114a29fe364063a0
fferrlop <fferrlop@myuax.com>
2023-12-12 12:09:45 +0100

    2

--- main.cpp
+++ main.cpp
@@ -7,8 +7,27 @@ struct Estudiante {
     float promedio;
 };
 
+union Dato {
+    int i;
+    float f;
+    char str[20];
+};
+
+
 
 int main() {
+
+    union Dato dato;
+
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
+
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
+
+    strcpy(dato.str, "C programming");
+    printf("dato.str: %s\n", dato.str);
+
     struct Estudiante estudiante1;
     strcpy(estudiante1.nombre, "Juan");
     estudiante1.edad = 20;
