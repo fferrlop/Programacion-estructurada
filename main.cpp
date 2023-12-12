cfd45bd82c6b61a80f0e3b9553038ade118cfd8b
fferrlop <fferrlop@myuax.com>
2023-12-12 11:45:56 +0100

    structura estudiante

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
