082244f90e4e989b3a41710dada9a4217cd7a536
fferrlop <fferrlop@myuax.com>
2023-12-12 11:55:51 +0100

    main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@ struct Estudiante {
     float promedio;
 };
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
     return 0;
 }
